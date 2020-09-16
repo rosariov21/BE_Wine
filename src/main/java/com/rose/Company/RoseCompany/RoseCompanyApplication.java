6081a3faede5289d46425c37ee86a44790aa1826
rosariov21 <rosariovelazquez@Rosarios-MBP.home>
2020-09-16 17:38:48 -0400

    First-Commit

---
+++ src/main/java/com/rose/Company/RoseCompany/RoseCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.rose.Company.RoseCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoseCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoseCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rose/Company/RoseCompany/RoseCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rose.Company.RoseCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoseCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
