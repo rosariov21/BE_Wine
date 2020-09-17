04368800dacc23c9c3b2d55dfee4b199cf72b6aa
rosariov21 <rosariovelazquez@Rosarios-MBP.home>
2020-09-17 14:22:30 -0400

    again

--- src/main/java/com/rose/Company/RoseCompany/EmployeeRESTController.java
+++ src/main/java/com/rose/Company/RoseCompany/EmployeeRESTController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 //
 //
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/rose_api/v1")
 
